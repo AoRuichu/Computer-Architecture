874a66c33a9b18b18d565f73cd7b5dfaf08fb11d
Zhiyong Wang <r0926826@pc-klas2-12.esat.kuleuven.be>
2025-04-13 18:21:22 +0200

    many test files

---
+++ CA_Exercises/SIM/files_verilog_MULT1.f
@@ -0,0 +1,16 @@
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/alu.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/alu_control.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/branch_unit.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/control_unit.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/cpu.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/immediate_extend_unit.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/mux_2.v
+
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/pc.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/register_file.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/reg_arstn.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/reg_arstn_en.v
+../Verilog/RTL_SOLUTION1_simple_program_and_MULT1/sram.v
+../Verilog/cpu_tb.v
+../Verilog/sky130_sram_2rw.v
+
