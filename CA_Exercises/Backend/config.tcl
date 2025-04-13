6192153559821e7765b15b9acde0c4c8aa047e1e
Zhiyong Wang <r0926826@pc-klas2-12.esat.kuleuven.be>
2025-04-13 19:32:54 +0200

    moved

--- CA_Exercises/Backend/config.tcl
+++ CA_Exercises/Backend/config.tcl
@@ -11,25 +11,13 @@ set ::env(CLOCK_TREE_SYNTH) 1
 set ::env(DESIGN_IS_CORE) 1      
 set ::env(SYNTH_MAX_FANOUT) 30
 
-set ::env(SYNTH_STRATEGY) "AREA 2"
+set ::env(SYNTH_STRATEGY) "AREA 0"
 set ::env(SYNTH_NO_FLAT) 1
 
 # Connect the layout files and abstracts
 # Connect the blackbox information and timing data
-set ::env(EXTRA_LEFS)      [list OpenRAM_output/sky130_sram_2rw_32x128_32.lef OpenRAM_output/sky130_sram_2rw_64x128_64.lef]
-set ::env(EXTRA_GDS_FILES) [list OpenRAM_output/sky130_sram_2rw_64x128_64.gds OpenRAM_output/sky130_sram_2rw_32x128_32.gds]
+set ::env(EXTRA_LEFS)      [list OpenRAM_output/sky130_sram_2rw_64x128_64.lef OpenRAM_output/sky130_sram_2rw_32x128_32.lef]
+set ::env(EXTRA_GDS_FILES) [list OpenRAM_output/sky130_sram_2rw_32x128_32.gds OpenRAM_output/sky130_sram_2rw_64x128_64.gds]
 set ::env(EXTRA_LIBS)      [list OpenRAM_output/sky130_sram_2rw_64x128_64_TT_1p8V_25C.lib OpenRAM_output/sky130_sram_2rw_32x128_32_TT_1p8V_25C.lib]
 
 set ::env(BASE_SDC_FILE) H05d4a.sdc
-
-# SRAM config
-set ::env(VDD_NETS) "vccd1"
-set ::env(GND_NETS) "vssd1"
-set ::env(FP_PDN_MACRO_HOOKS) "\
- instruction_memory.*.dram_inst      vccd1 vssd1 vccd1 vssd1,\
- data_memory.*.spad_inst      vccd1 vssd1 vccd1 vssd1"
-
-# Some floorplan empirical parameters
-set ::env(FP_CORE_UTIL) 35
-set ::env(FP_SIZING) relative
-set ::env(PL_MACRO_CHANNEL) [list 80 80]
